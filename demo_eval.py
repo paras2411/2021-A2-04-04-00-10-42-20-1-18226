295a20bc3e0e0ab25ee0a1cad8b0a13cbd9f663d
paras2411 <lohaniparas1997@gmail.com>
2021-05-11 01:23:59 +0530

    Fix demo_eval.py

--- demo_eval.py
+++ demo_eval.py
@@ -12,6 +12,13 @@ SOS_token = 1  # Start-of-sentence token
 EOS_token = 2  # End-of-sentence token
 OOV_token = 3  # out of vocabulary token
 
+
+batch_size = 64  # Batch size for training.
+epochs = 100  # Number of epochs to train for.
+latent_dim = 256  # Latent dimensionality of the encoding space.
+num_samples = 10000  # Number of samples to train on.
+
+
 dim = 50
 
 
